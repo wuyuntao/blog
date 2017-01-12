c508d86d8bba322242c9ba24b26b5fcf324dc5e5
Wu Yuntao <wyt.brandon@gmail.com>
2017-01-12 19:03:34 +0800

    1, Update social accounts

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,14 +22,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('GitHub', 'https://github.com/wuyuntao'),
+    ('Twitter', 'https://twitter.com/wuyuntao'),
+)
 
 DEFAULT_PAGINATION = 10
 
